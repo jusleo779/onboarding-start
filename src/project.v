eea51cc347837938b0e2eb64052c5fadfa09d535
Justin Leong <justingleong@gmail.com>
2025-12-27 11:04:51 -0800

    project.v

--- src/project.v
+++ src/project.v
@@ -22,16 +22,16 @@ module tt_um_uwasic_onboarding_Justin_Leong(
     wire[7:0] en_reg_pwm_7_0;
     wire[7:0] en_reg_pwm_15_8;
     wire[7:0] pwm_duty_cycle;
-    pwm_peripheral pwm_peripheral_inst(
-        .clk(clk),
-        .rst_n(rst_n),
-        .en_reg_out_7_0(en_reg_out_7_0),
-        .en_reg_out_15_8(en_reg_out_15_8),
-        .en_reg_pwm_7_0(en_reg_pwm_7_0),
-        .en_reg_pwm_15_8(en_reg_pwm_15_8),
-        .pwm_duty_cycle(pwm_duty_cycle),
-        .out({uio_out, uo_out})
-);
+   pwm_peripheral pwm_peripheral_inst (
+    .clk(clk),
+    .rst_n(rst_n),
+    .en_reg_out_7_0(en_reg_out_7_0),
+    .en_reg_out_15_8(en_reg_out_15_8),
+    .en_reg_pwm_7_0(en_reg_pwm_7_0),
+    .en_reg_pwm_15_8(en_reg_pwm_15_8),
+    .pwm_duty_cycle(pwm_duty_cycle),
+    .out({uio_out, uo_out})
+  );
    
  
   // All output pins must be assigned. If not used, assign to 0. 
