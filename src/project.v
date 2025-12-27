723e38f66363f39b3f95d18b8658efc97437996c
Justin Leong <justingleong@gmail.com>
2025-12-27 10:55:25 -0800

    project.v

--- src/project.v
+++ src/project.v
@@ -34,10 +34,9 @@ module tt_um_uwasic_onboarding_Justin_Leong(
 );
    
  
-  // All output pins must be assigned. If not used, assign to 0.
+  // All output pins must be assigned. If not used, assign to 0. 
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+ 
 
   // List all unused inputs to prevent warnings
    wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
